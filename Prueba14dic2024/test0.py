fcf64b54c0fbbfd14372291b3c431b38594889f6
Fabian Montenegro <faromoqui@gmail.com>
2024-12-14 13:51:28 -0500

    1:51

---
+++ Prueba14dic2024/test0.py
@@ -0,0 +1,12 @@
+
+def sumar(a, b):
+	return a + b #cambiar función
+
+res = sumar(3, 4)
+print(res)
+
+try:
+    assert sumar(3, 4.) == 7
+    print("La aserción pasó correctamente.")
+except AssertionError:
+    print("La aserción falló.")
